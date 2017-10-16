3b4756a7ce4e8a84cd326913fe5a9230c25cb448
Todd Mantell <toddmantell@gmail.com>
2017-10-15 20:07:29 -0500

    added post and put methods; extracted getAllProjects which needs async/await for promise handling; added baseUrl to config

--- src/app.js
+++ src/app.js
@@ -2,12 +2,12 @@ const app = require('express')();
 const bodyParser = require('body-parser');
 const db = require('./dataStore/db');
 const Project = require('./dataStore/ProjectSchema');
+const {baseUrl} = require('./config');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());//without this, application/json will not get processed https://stackoverflow.com/questions/38294730/express-js-post-req-body-empty
 app.use((req, res, next) => {
-  console.log(`Request for ${req.path} received from ${req.ip} at ${new Date()}`);
-  console.log('body', req.body);  
+  console.log(`Request for ${req.path} received from ${req.ip} at ${new Date()}`);  
   next();
 });
 
@@ -15,18 +15,22 @@ app.get('/', (req, res, next) => {
   res.send('Service is up!');
 });
 
-app.get('/projects', (req, res) => {
-  Project.find({}, (error, projects) => {
-    if (error) return res.status(500).send("There was a problem finding projects");
-    return res.status(200).send(projects);
-  });
+app.get('/projects', async (req, res) => {
+  const allProjects = await getAllProjects();
+
+  if (allProjects.length) return res.status(200).send(allProjects);
+  return res.status(500).send("There was a problem retrieving projects.");
 });
 
-app.post('/projects/', (req, res) => {
-  const newProject = req.body;
+app.post('/projects/', async (req, res) => {
+  const allProjects = await getAllProjects();
 
-  console.log(newProject);
-  res.status(201).send('ok');
+  const newProject = Object.assign({}, req.body, {id: allProjects.length + 1});
+
+  Project.create(newProject, (error, project) => {
+    if (error) return res.status(500).send("There was an error when attempting to add the new project.");
+    return res.status(201).send(`${baseUrl}/projects/${newProject.id}`);
+  });
 });
 
 app.get('/projects/:id', (req, res) => {
@@ -49,11 +53,18 @@ app.put('/projects/:id', (req, res) => {
   });
 });
 
-app.post('/projects/', (req, res) => {
-  const id = checkForValidId(req.params.id);
-
-  projects.push({})// Would be nice to not push.  Also need the request body, not the id
-});
+async function getAllProjects() {  
+  const allProjects = await Project.find({}, (error, projects) => {
+    if (error) {
+      console.log('error: ', error);
+      throw error;
+    } else {
+      return projects;
+    }
+  });
+  
+  return allProjects;
+}
 
 function checkForValidId(id) {
   return parseInt(id) || 0;// checks to see if left side is truthy, if yes return it, otherwise return what is on the right
