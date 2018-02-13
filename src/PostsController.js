b1f85e179935632640d541ab33407733bb562ba1
Todd Mantell <tmantell99@yahoo.com>
2018-02-12 22:46:20 -0600

    Modified ProjectsController to PostsController

--- src/PostsController.js
+++ src/PostsController.js
@@ -1,71 +1,71 @@
 const Router = require('express').Router();
 const db = require('./dataStore/db');
-const Project = require('./dataStore/ProjectSchema');
+const Post = require('./dataStore/PostSchema');
 const {baseUrl} = require('./config');
 
 
 Router.get('/', async (req, res) => {
-  const allProjects = await getAllProjects();
+  const allPosts = await getAllPosts();
 
-  if (allProjects.length) return res.status(200).send(allProjects);
-  return res.status(500).send("There was a problem retrieving projects.");
+  if (allPosts.length) return res.status(200).send(allPosts);
+  return res.status(500).send("There was a problem retrieving posts.");
 });
 
 Router.post('/', async (req, res) => {
-  const allProjects = await getAllProjects();
+  const allPosts = await getAllPosts();
 
-  const lastProject = allProjects[allProjects.length - 1];
+  const lastPost = allPosts[allPosts.length - 1];
 
-  const newProject = Object.assign({}, req.body, {id: lastProject.id + 1});
+  const newPost = Object.assign({}, req.body, {id: lastPost.id + 1});
 
-  Project.create(newProject, (error, project) => {
-    if (error) return res.status(500).send("There was an error when attempting to add the new project.");
-    return res.status(201).json(newProject);
+  Post.create(newPost, (error, post) => {
+    if (error) return res.status(500).send("There was an error when attempting to add the new post.");
+    return res.status(201).json(newPost);
   });
 });
 
 Router.get('/:id', (req, res) => {
   const id = checkForValidId(req.params.id);
 
-  Project.find({id}, (error, project) => {
-    if (project.length) return res.status(200).send(project);
-    return res.status(404).send("Project not found.");
-    
+  Post.find({id}, (error, post) => {
+		if (post.length) return res.status(200).send(post);
+		
+    return res.status(404).send("Post not found.");
   });
 });
 
 Router.put('/:id', (req, res) => {
   const id = checkForValidId(req.params.id);
 
-  Project.findOne({id}, (error, project) => {
-    if (error) return res.status(404).send("Project not found.");
-    else if (project) { 
-      updateProject(project, req, res);
+  Post.findOne({id}, (error, post) => {
+    if (error) return res.status(404).send("Post not found.");
+    else if (post) { 
+      updatePost(post, req, res);
     }
   });
 });
 
-async function getAllProjects() {  
-  const allProjects = await Project.find({}, (error, projects) => {
+async function getAllPosts() {  
+  const allPosts = await Post.find({}, (error, posts) => {
     if (error) {
       console.log('error: ', error);
       throw error;
     } else {
-      return projects;
+      return posts;
     }
   });
   
-  return allProjects;
+  return allPosts;
 }
 
 function checkForValidId(id) {
   return parseInt(id) || 0;// checks to see if left side is truthy, if yes return it, otherwise return what is on the right
 }
 
-function updateProject(project, req, res) {
-  Project.findByIdAndUpdate(project._id, req.body, {new: true}, (error, project) => {
-    if (error) return res.status(500).send("Unable to update project.");
-    res.status(200).send(project);
+function updatePost(post, req, res) {
+  Post.findByIdAndUpdate(post._id, req.body, {new: true}, (error, post) => {
+    if (error) return res.status(500).send("Unable to update post.");
+    res.status(200).send(post);
   });
 }
 
