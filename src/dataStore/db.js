80ccba0b9320fed5342d5fe754b8d5064d34bd42
todder02 <tmantell99@yahoo.com>
2018-08-30 11:03:28 -0500

    added .env

--- src/dataStore/db.js
+++ src/dataStore/db.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;//Set mongoose to use native JS Promises
 
-mongoose.connect('mongodb://a-mantell:whatwhatyo@ds127842.mlab.com:27842/apitest',
+const {env} = process;
+
+mongoose.connect(`mongodb://${env.DB_USER}:${env.DB_PASSWORD}@ds127842.mlab.com:27842/apitest`,
   {useMongoClient: true}
 );
